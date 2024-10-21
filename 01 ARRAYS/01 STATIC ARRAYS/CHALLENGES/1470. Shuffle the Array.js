da05b34ce0c71834665f72a281a75eec9803ce41
fakhranyy <ziad.fakhranyy@gmail.com>
2024-10-21 19:10:03 +0300

    STATIC ARRAYS

--- 01 ARRAYS/01 STATIC ARRAYS/CHALLENGES/1470. Shuffle the Array.js
+++ 01 ARRAYS/01 STATIC ARRAYS/CHALLENGES/1470. Shuffle the Array.js
@@ -32,8 +32,17 @@ nums.length == 2n
  * @param {number} n
  * @return {number[]}
  */
-var shuffle = function (nums, n) {};
+var shuffle = function (nums, n) {
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(nums[i]);
+    arr.push(nums[i + n]);
+  }
+  return arr;
+};
 // Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7
+// n = 3
 //  x1 , y1 , x2 , y2 , x3 , y3
 
 console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
+// Output: [2,3,5,4,1,7]
